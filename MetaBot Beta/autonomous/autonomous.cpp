287d6192c4882afd2830d5a14c76d50022506c19
Caleb <81331353+Darksabre1500@users.noreply.github.com>
2022-11-02 17:13:35 -0500

    Try this for auton consistency

--- MetaBot Beta/autonomous/autonomous.cpp
+++ MetaBot Beta/autonomous/autonomous.cpp
@@ -12,6 +12,33 @@
 
 void autonomous(void) {
   flywheel(true, 550);
+  waitUntil((Flywheel1.velocity(rpm) >= 520) || (Flywheel2.velocity(rpm) >= 520));
+  shootDisk();
+  waitUntil((Flywheel1.velocity(rpm) >= 520) || (Flywheel2.velocity(rpm) >= 520));
+  shootDisk();
+  wait(1, sec);
+  flywheel(false, 520);
+  moveForward(2.5, 2);
+  turnTo(230, 2);
+  wait(0.25, sec);
+  strafeRight(1.5, 2);
+  moveForward(8.3, 1);
+  intake(true, reverse);
+  wait(2, sec);
+  intake(false, reverse);
+
+  /*flywheel(true, 520);
+  wait(2.9, sec);
+  shootDisk();
+  wait(1.3, sec);
+  shootDisk();
+  wait(1, sec);
+  flywheel(false, 520);*/
+
+
+  waitUntil((Flywheel1.velocity(rpm) >= 520) || (Flywheel2.velocity(rpm) >= 520));
+  /*
+  flywheel(true, 550);
   wait(2.5, sec);
   shootDisk();
   wait(1.2, sec);
@@ -26,24 +53,6 @@ void autonomous(void) {
   intake(true, reverse);
   wait(2, sec);
   intake(false, reverse);
-  //strafeRight(3, 2);
-  //moveForward(5, 2);
-  //intake(true, reverse);
-  //goTo(3, 10, 2, ABSOLUTE);
-  //goTo(0, 12, 2, RELATIVE);
-  //turnTo(165, 2);
+  */ 
 
-  /*flywheel(true, 520);
-  wait(3, sec);
-  shootDisk();
-  wait(2, sec);
-  shootDisk();*/
-
-  /*flywheel(true, 520);
-  wait(2.9, sec);
-  shootDisk();
-  wait(1.3, sec);
-  shootDisk();
-  wait(1, sec);
-  flywheel(false, 520);*/
 } 
