ef462cbb8a76d3866d7cb422ec914591523371e2
Caleb <calebbev1@gmail.com>
2023-01-10 14:55:04 -0600

    Added Auton

--- MetaBot Beta/autonomous/autonomous.cpp
+++ MetaBot Beta/autonomous/autonomous.cpp
@@ -12,55 +12,25 @@
 
 void autonomous(void) {
   
-  //Flywheel
-  flywheel(true, 450);
-  waitUntil((Flywheel1.velocity(rpm) >= 450) || (Flywheel2.velocity(rpm) >= 450));
-  shootDisk();
-  wait(0.5, sec);
-  flywheel(true, 450);
-  waitUntil((Flywheel1.velocity(rpm) >= 450) || (Flywheel2.velocity(rpm) >= 450));
-  shootDisk();
-  wait(0.5, sec);
-  
   //Roller
-  flywheel(false, 420);
-  move(2, fwd, 2);
-  turnTo(240, 2);
-  wait(0.25, sec);
-  strafe(6, right, 2);
-  wait(0.25, sec);
+  move(0.5, fwd, 1);
+  wait(0.5, sec);
   intake(true, reverse);
-  move(8.5, fwd, 2);
-  wait(1.5, sec);
-  intake(false, reverse);
-
-
-  /*flywheel(true, 520);
-  wait(2.9, sec);
-  shootDisk();
-  wait(1.3, sec);
-  shootDisk();
   wait(1, sec);
-  flywheel(false, 520);*/
-
-
-  //waitUntil((Flywheel1.velocity(rpm) >= 520) || (Flywheel2.velocity(rpm) >= 520));
-  /*
-  flywheel(true, 550);
-  wait(2.5, sec);
-  shootDisk();
-  wait(1.2, sec);
-  shootDisk();
-  wait(1.5, sec);
-  flywheel(false, 520);
-  moveForward(2.5, 2);
-  turnTo(230, 2);
-  wait(0.25, sec);
-  strafeRight(1, 2);
-  moveForward(8.3, 1);
-  intake(true, reverse);
-  wait(2, sec);
   intake(false, reverse);
-  */ 
+
+  //Shoot
+  move(2, reverse, 0.4);
+  turnTo(150, 2);
+  flywheel(true, 400);
+  waitUntil((Flywheel1.velocity(rpm) >= 400) || (Flywheel2.velocity(rpm) >= 400));
+  wait(0.3, sec);
+  shootDisk();
+  wait(0.5, sec);
+  waitUntil((Flywheel1.velocity(rpm) >= 400) || (Flywheel2.velocity(rpm) >= 400));
+  wait(0.3, sec);
+  shootDisk();
+  wait(0.5, sec);
+  flywheel(false, 400);
 
 } 
