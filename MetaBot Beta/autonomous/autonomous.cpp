fd2e17bd265f8d9c3d98436f23161d113ab792b6
Caleb <calebbev1@gmail.com>
2023-01-14 08:13:16 -0600

    Power Issues

--- MetaBot Beta/autonomous/autonomous.cpp
+++ MetaBot Beta/autonomous/autonomous.cpp
@@ -13,25 +13,32 @@
 void autonomous(void) {
   
   //Flywheel
-  flywheel(true, 450);
-  waitUntil((Flywheel1.velocity(rpm) >= 450) || (Flywheel2.velocity(rpm) >= 450));
+  flywheel(true, 500);
+  waitUntil((Flywheel1.velocity(rpm) >= 500) || (Flywheel2.velocity(rpm) >= 500));
   shootDisk();
   wait(0.5, sec);
-  flywheel(true, 450);
-  waitUntil((Flywheel1.velocity(rpm) >= 450) || (Flywheel2.velocity(rpm) >= 450));
+  flywheel(true, 490);
+  waitUntil((Flywheel1.velocity(rpm) <= 490) || (Flywheel2.velocity(rpm) <= 490));
   shootDisk();
   wait(0.5, sec);
   
   //Roller
   flywheel(false, 420);
-  move(2, fwd, 2);
-  turnTo(240, 2);
+  strafe(1.5, right, 0.75);
+  turnTo(70, 1);
+  move(15, reverse, 0.75);
+  odom.resetAngle();
+  move(8, fwd, 2);
   wait(0.25, sec);
-  strafe(6, right, 2);
+  turnTo(270, 2);
+  wait(0.25, sec);
+  strafe(13, right, 2);
+  wait(0.25, sec);
+  turnTo(267, 2);
   wait(0.25, sec);
   intake(true, reverse);
-  move(8.5, fwd, 2);
-  wait(1.5, sec);
+  move(7.5, fwd, 2);
+  wait(0.5, sec);
   intake(false, reverse);
 
 
