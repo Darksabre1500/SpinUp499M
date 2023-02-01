4b09896be1f5e0dc606511b887c9b3b8dfea6bb3
Caleb <calebbev1@gmail.com>
2023-02-01 15:55:02 -0600

    Removed Auton for main template

--- MetaBot Beta/autonomous/autonomous.cpp
+++ MetaBot Beta/autonomous/autonomous.cpp
@@ -11,23 +11,5 @@
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  
-  //Roller
-  move(2, fwd, 1);
-  roller(RED);
-
-  //Shoot
-  move(1.5, reverse, 1);
-  turnTo(170, 2);
-  flywheel(true, 350);
-  waitUntil((Flywheel1.velocity(rpm) >= 350) || (Flywheel2.velocity(rpm) >= 350));
-  wait(0.3, sec);
-  shootDisk();
-  wait(0.5, sec);
-  waitUntil((Flywheel1.velocity(rpm) >= 350) || (Flywheel2.velocity(rpm) >= 350));
-  wait(0.3, sec);
-  shootDisk();
-  wait(0.5, sec);
-  flywheel(false, 400);
 
 } 
