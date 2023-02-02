f59fb22e6b6c084081e0cd11b2baaea49e30f48c
Caleb <81331353+Darksabre1500@users.noreply.github.com>
2023-02-01 20:44:44 -0600

    Implemented Auton with new vision Roller

--- MetaBot Beta/autonomous/autonomous.cpp
+++ MetaBot Beta/autonomous/autonomous.cpp
@@ -10,6 +10,32 @@
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-void autonomous(void) {
-
+void autonomous(void) {  
+  //Flywheel
+  flywheel(true, 600);
+  waitUntil((Flywheel1.velocity(rpm) >= 600) || (Flywheel2.velocity(rpm) >= 600));
+  shootDisk();
+  wait(0.5, sec);
+  flywheel(true, 600);
+  waitUntil((Flywheel1.velocity(rpm) <= 600) || (Flywheel2.velocity(rpm) <= 600));
+  shootDisk();
+  wait(0.5, sec);
+  
+  //Roller
+  flywheel(false, 420);
+  strafe(1.5, right, 0.75);
+  turnTo(70, 1);
+  move(15, reverse, 0.75);
+  odom.resetAngle();
+  move(8, fwd, 2);
+  wait(0.25, sec);
+  turnTo(270, 2);
+  wait(0.25, sec);
+  strafe(13, right, 2);
+  wait(0.25, sec);
+  turnTo(267, 2);
+  wait(0.25, sec);
+  move(7.5, fwd, 2);
+  wait(0.25, sec);
+  roller(colorFinder());
 } 
