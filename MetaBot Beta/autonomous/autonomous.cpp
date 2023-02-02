a42996eca68ae80b71d34d104751bad7eccb3f8e
Caleb <calebbev1@gmail.com>
2023-02-02 15:46:12 -0600

    Update autonomous.cpp

--- MetaBot Beta/autonomous/autonomous.cpp
+++ MetaBot Beta/autonomous/autonomous.cpp
@@ -19,7 +19,7 @@ void autonomous(void) {
 
   //Shoot
   move(1.5, reverse, 1);
-  turnTo(170, 2);
+  turnTo(175, 2);
   flywheel(true, 350);
   waitUntil((Flywheel1.velocity(rpm) >= 350) || (Flywheel2.velocity(rpm) >= 350));
   wait(0.3, sec);
